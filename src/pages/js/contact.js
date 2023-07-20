266b558b117f8ba0b4917f1c81398f6400c4be5d
Davis-Cedrick Baseka <base0005@algonquinlive.com>
2023-07-20 05:29:06 -0400

    finished Full Mobile + Better BG

--- src/pages/js/contact.js
+++ src/pages/js/contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "../css/contact.css"
+// import BGImage from "../../img/Drip_Logo_Pattern.jpg"
 
 export default function contact(){
     return(
         <div className="wholeContact">
+            {/* <img className="BGPattern" src={BGImage}/> */}
             <div className="ContactTitle">
                 Contact Me Here
             </div>
